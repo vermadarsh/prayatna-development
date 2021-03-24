41af58b37b191d09827ab3ae9edb674a45bba8f6
Adarsh Verma <adarsh.srmcem@gmail.com>
2021-03-25 00:05:19 +0530

    added

--- wp-content/plugins/core-functions/includes/core-functions.php
+++ wp-content/plugins/core-functions/includes/core-functions.php
@@ -36,7 +36,7 @@ function cf_get_time_based_text() {
 			the_row();
 			$start_time = get_sub_field( 'start_time' );
 			$end_time   = get_sub_field( 'end_time' );
-			$is_between = cognify_time_is_between_two_times( $start_time, $end_time, $current_time );
+			$is_between = cf_time_is_between_two_times( $start_time, $end_time, $current_time );
 
 			if ( $is_between ) {
 				$text = get_sub_field( 'time_dependant_text' );
@@ -45,4 +45,24 @@ function cf_get_time_based_text() {
 	}
 
 	return $text;
+}
+
+/**
+ * Check if a time falls in between 2 time values. Format - H:i:s
+ *
+ * @param $from string Holds the from time.
+ * @param $till string Holds the till time.
+ * @param $input string Holds the input time.
+ * @return boolean
+ */
+function cf_time_is_between_two_times( $from, $till, $input ) {
+	$f = DateTime::createFromFormat( 'H:i:s', $from );
+	$t = DateTime::createFromFormat( 'H:i:s', $till );
+	$i = DateTime::createFromFormat( 'H:i:s', $input );
+
+	if ( $f > $t ) {
+		$t->modify('+1 day');
+	}
+
+	return ( $f <= $i && $i <= $t ) || ( $f <= $i->modify( '+1 day' ) && $i <= $t );
 }
