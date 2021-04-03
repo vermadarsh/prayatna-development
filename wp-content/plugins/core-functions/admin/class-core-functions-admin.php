da928d6325c33211442d43acf492f3f7db217f0e
Adarsh Verma <adarsh.srmcem@gmail.com>
2021-04-03 20:27:42 +0530

    added

--- wp-content/plugins/core-functions/admin/class-core-functions-admin.php
+++ wp-content/plugins/core-functions/admin/class-core-functions-admin.php
@@ -173,16 +173,19 @@ class Core_Functions_Admin {
 			$role = get_role( $_role );
 
 			// $role->add_cap( 'read' );
-			// $role->add_cap( 'read_client-log');
-			// $role->add_cap( 'read_private_client-logs' );
-			// $role->add_cap( 'edit_client-log' );
-			// $role->add_cap( 'edit_client-logs' );
-			// $role->add_cap( 'edit_others_client-logs' );
-			// $role->add_cap( 'edit_published_client-logs' );
-			// $role->add_cap( 'publish_client-logs' );
-			// $role->add_cap( 'delete_others_client-logs' );
-			// $role->add_cap( 'delete_private_client-logs' );
-			// $role->add_cap( 'delete_published_client-logs' );
+			$role->add_cap( 'read_client-log');
+			$role->add_cap( 'read_private_client-logs' );
+			$role->add_cap( 'edit_client-log' );
+			$role->add_cap( 'edit_client-logs' );
+			$role->add_cap( 'edit_others_client-logs' );
+			$role->add_cap( 'edit_private_client-logs' );
+			$role->add_cap( 'edit_published_client-logs' );
+			$role->add_cap( 'publish_client-logs' );
+			$role->add_cap( 'delete_others_client-logs' );
+			$role->add_cap( 'delete_others_client-logs' );
+			$role->add_cap( 'delete_client-log' );
+			$role->add_cap( 'delete_client-logs' );
+			$role->add_cap( 'delete_published_client-logs' );
 		}
 	}
 }
