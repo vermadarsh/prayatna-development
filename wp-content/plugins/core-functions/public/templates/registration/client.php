31c9abf6d8b9152a993dfb15d34a471ee2d2e027
Adarsh Verma <adarsh.srmcem@gmail.com>
2021-04-03 15:47:59 +0530

    added

--- wp-content/plugins/core-functions/public/templates/registration/client.php
+++ wp-content/plugins/core-functions/public/templates/registration/client.php
@@ -17,7 +17,7 @@ defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 			<!-- FIRST NAME -->
 			<div class="col-md-6">
 				<span class="input input--hfd">
-					<input class="input__field input__field--hfd" type="text" name="client-first-name" id="client-first-name" required onkeypress="return /[a-z]/i.test(event.key)" />
+					<input class="input__field input__field--hfd" type="text" name="client-first-name" id="client-first-name" onkeypress="return /[a-z]/i.test(event.key)" />
 					<label class="input__label input__label--hfd" for="client-first-name">
 						<span class="input__label-content input__label-content--hfd "><?php esc_html_e( 'First Name*', 'core-functions' ); ?></span>
 					</label>
@@ -26,7 +26,7 @@ defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 			<!-- LAST NAME -->
 			<div class="col-md-6">
 				<span class="input input--hfd">
-					<input class="input__field input__field--hfd" type="text" name="client-last-name" id="client-last-name" required onkeypress="return /[a-z]/i.test(event.key)" />
+					<input class="input__field input__field--hfd" type="text" name="client-last-name" id="client-last-name" onkeypress="return /[a-z]/i.test(event.key)" />
 					<label class="input__label input__label--hfd" for="client-last-name">
 						<span class="input__label-content input__label-content--hfd "><?php esc_html_e( 'Last Name*', 'core-functions' ); ?></span>
 					</label>
@@ -35,7 +35,7 @@ defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 			<!-- PHONE NUMBER -->
 			<div class="col-md-6 top__margin__10 cf_user_registration_phone_field">
 				<span class="input input--hfd">
-					<input class="input__field input__field--hfd" type="text" maxlength="10" name="client-phone" id="client-phone" required onkeypress="return /[0-9]/i.test(event.key)" />
+					<input class="input__field input__field--hfd" type="text" maxlength="10" name="client-phone" id="client-phone" onkeypress="return /[0-9]/i.test(event.key)" />
 					<label class="input__label input__label--hfd" for="client-phone">
 						<span class="input__label-content input__label-content--hfd "><?php esc_html_e( 'Phone*', 'core-functions' ); ?></span>
 					</label>
@@ -44,7 +44,7 @@ defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 			<!-- PASSWORD -->
 			<div class="col-md-6 top__margin__10">
 				<span class="input input--hfd">
-					<input class="input__field input__field--hfd" type="password" name="client-password" id="client-password" required />
+					<input class="input__field input__field--hfd" type="password" name="client-password" id="client-password" />
 					<label class="input__label input__label--hfd" for="client-password">
 						<span class="input__label-content input__label-content--hfd "><?php esc_html_e( 'Password*', 'core-functions' ); ?></span>
 					</label>
@@ -57,7 +57,7 @@ defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 			<!-- EMAIL -->
 			<div class="col-md-12 top__margin__10 cf_user_registration_email_field">
 				<span class="input input--hfd">
-					<input class="input__field input__field--hfd" type="email" name="client-email" id="client-email" required />
+					<input class="input__field input__field--hfd" type="email" name="client-email" id="client-email" />
 					<label class="input__label input__label--hfd" for="client-email">
 						<span class="input__label-content input__label-content--hfd "><?php esc_html_e( 'Email*', 'core-functions' ); ?></span>
 					</label>
@@ -65,11 +65,11 @@ defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 			</div>
 			<!-- TEMPORARY ADDRESS -->
 			<div class="col-md-6 top__margin__10">
-				<textarea required name="client-temporary-address" placeholder="<?php esc_html_e( 'Temporary address', 'cognify-core' ); ?>" class="form-control therapist-temporary-address" rows="3"></textarea>
+				<textarea placeholder="<?php esc_html_e( 'Temporary address', 'cognify-core' ); ?>" class="form-control client-temporary-address" rows="3"></textarea>
 			</div>
 			<!-- PERMANENT ADDRESS -->
 			<div class="col-md-6 top__margin__10">
-				<textarea required name="client-permanent-address" placeholder="<?php esc_html_e( 'Permanent address', 'cognify-core' ); ?>" class="form-control therapist-permanent-address" rows="3"></textarea>
+				<textarea placeholder="<?php esc_html_e( 'Permanent address', 'cognify-core' ); ?>" class="form-control client-permanent-address" rows="3"></textarea>
 			</div>
 		</div>
 		<h4 class="top__margin__20"><?php esc_html_e( 'Child/Children Basic Details', 'core-functions' ); ?></h4>
@@ -84,7 +84,7 @@ defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 			<!-- TERMS & CONDITIONS ACCEPTANCE -->
 			<div class="col-md-12 top__margin__10">
 				<div class="custom-control custom-checkbox client-registration-acceptance">
-					<input type="checkbox" class="custom-control-input" id="client-registration-terms-n-conditions-acceptance" required />
+					<input type="checkbox" class="custom-control-input" id="client-registration-terms-n-conditions-acceptance" />
 					<label class="custom-control-label" for="client-registration-terms-n-conditions-acceptance">
 						<?php echo sprintf( __( 'By clicking Sign Up, you agree to our %3$sTerms of Services%2$s and %1$sPrivacy Policy%2$s. You may receive SMS/Email notifications from us and can opt out at any time.', 'core-functions' ), '<a class="" href="/privacy-policy/">', '</a>', '<a class="" href="/terms-of-services-client/">' ); ?>
 					</label>
--- wp-content/plugins/core-functions/public/templates/registration/therapist.php
+++ wp-content/plugins/core-functions/public/templates/registration/therapist.php
@@ -16,7 +16,7 @@ defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 			<!-- FIRST NAME -->
 			<div class="col-md-6">
 				<span class="input input--hfd">
-					<input class="input__field input__field--hfd" type="text" name="therapist-first-name" id="therapist-first-name" required onkeypress="return /[a-z]/i.test(event.key)" />
+					<input class="input__field input__field--hfd" type="text" name="therapist-first-name" id="therapist-first-name" onkeypress="return /[a-z]/i.test(event.key)" />
 					<label class="input__label input__label--hfd" for="therapist-first-name">
 						<span class="input__label-content input__label-content--hfd "><?php esc_html_e( 'First Name*', 'core-functions' ); ?></span>
 					</label>
@@ -25,7 +25,7 @@ defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 			<!-- LAST NAME -->
 			<div class="col-md-6">
 				<span class="input input--hfd">
-					<input class="input__field input__field--hfd" type="text" name="therapist-last-name" id="therapist-last-name" required onkeypress="return /[a-z]/i.test(event.key)" />
+					<input class="input__field input__field--hfd" type="text" name="therapist-last-name" id="therapist-last-name" onkeypress="return /[a-z]/i.test(event.key)" />
 					<label class="input__label input__label--hfd" for="therapist-last-name">
 						<span class="input__label-content input__label-content--hfd "><?php esc_html_e( 'Last Name*', 'core-functions' ); ?></span>
 					</label>
@@ -34,7 +34,7 @@ defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 			<!-- PHONE NUMBER -->
 			<div class="col-md-6 top__margin__10 cf_user_registration_phone_field">
 				<span class="input input--hfd">
-					<input class="input__field input__field--hfd" type="text" maxlength="10" name="therapist-phone" id="therapist-phone" required onkeypress="return /[0-9]/i.test(event.key)" />
+					<input class="input__field input__field--hfd" type="text" maxlength="10" name="therapist-phone" id="therapist-phone" onkeypress="return /[0-9]/i.test(event.key)" />
 					<label class="input__label input__label--hfd" for="therapist-phone">
 						<span class="input__label-content input__label-content--hfd "><?php esc_html_e( 'Phone*', 'core-functions' ); ?></span>
 					</label>
@@ -43,7 +43,7 @@ defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 			<!-- PASSWORD -->
 			<div class="col-md-6 top__margin__10">
 				<span class="input input--hfd">
-					<input class="input__field input__field--hfd" type="password" name="therapist-password" id="therapist-password" required />
+					<input class="input__field input__field--hfd" type="password" name="therapist-password" id="therapist-password" />
 					<label class="input__label input__label--hfd" for="therapist-password">
 						<span class="input__label-content input__label-content--hfd "><?php esc_html_e( 'Password*', 'core-functions' ); ?></span>
 					</label>
@@ -56,7 +56,7 @@ defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 			<!-- EMAIL -->
 			<div class="col-md-12 top__margin__10 cf_user_registration_email_field">
 				<span class="input input--hfd">
-					<input class="input__field input__field--hfd" type="email" name="therapist-email" id="therapist-email" required />
+					<input class="input__field input__field--hfd" type="email" name="therapist-email" id="therapist-email" />
 					<label class="input__label input__label--hfd" for="therapist-email">
 						<span class="input__label-content input__label-content--hfd "><?php esc_html_e( 'Email*', 'core-functions' ); ?></span>
 					</label>
@@ -65,7 +65,7 @@ defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 			<!-- DOB -->
 			<div class="col-md-6 top__margin__10">
 				<span class="input input--hfd">
-					<input class="input__field input__field--hfd cf__date__field" type="text" name="therapist-dob" id="therapist-dob" required />
+					<input class="input__field input__field--hfd cf__date__field" type="text" name="therapist-dob" id="therapist-dob" />
 					<label class="input__label input__label--hfd" for="therapist-dob">
 						<span class="input__label-content input__label-content--hfd "><?php esc_html_e( 'DOB* (DD-MM-YYYY)', 'core-functions' ); ?></span>
 					</label>
@@ -82,23 +82,23 @@ defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 			</div>
 			<!-- TEMPORARY ADDRESS -->
 			<div class="col-md-6 top__margin__10">
-				<textarea required name="therapist-temporary-address" placeholder="<?php esc_html_e( 'Temporary address', 'cognify-core' ); ?>" class="form-control therapist-temporary-address" rows="3"></textarea>
+				<textarea placeholder="<?php esc_html_e( 'Temporary address', 'cognify-core' ); ?>" class="form-control therapist-temporary-address" rows="3"></textarea>
 			</div>
 			<!-- PERMANENT ADDRESS -->
 			<div class="col-md-6 top__margin__10">
-				<textarea required name="therapist-permanent-address" placeholder="<?php esc_html_e( 'Permanent address', 'cognify-core' ); ?>" class="form-control therapist-permanent-address" rows="3"></textarea>
+				<textarea placeholder="<?php esc_html_e( 'Permanent address', 'cognify-core' ); ?>" class="form-control therapist-permanent-address" rows="3"></textarea>
 			</div>
 			<!-- PROFILE PICTURE -->
 			<div class="col-md-12 top__margin__10">
 				<div class="custom-file therapist-profile-picture">
-					<input type="file" class="custom-file-input" id="therapist-profile-picture" name="therapist-profile-picture" required>
+					<input type="file" class="custom-file-input" id="therapist-profile-picture" name="therapist-profile-picture">
 					<label class="custom-file-label" for="therapist-profile-picture"><?php esc_html_e( 'Upload Your Profile Picture. (maximum size 500Kb)*', 'core-functions' ); ?></label>
 				</div>
 			</div>
 			<!-- TERMS & CONDITIONS ACCEPTANCE -->
 			<div class="col-md-12 top__margin__10">
 				<div class="custom-control custom-checkbox therapist-registration-acceptance">
-					<input type="checkbox" class="custom-control-input" id="therapist-registration-terms-n-conditions-acceptance" required />
+					<input type="checkbox" class="custom-control-input" id="therapist-registration-terms-n-conditions-acceptance" />
 					<label class="custom-control-label" for="therapist-registration-terms-n-conditions-acceptance">
 						<?php echo sprintf( __( 'By clicking Sign Up, you agree to our %3$sTerms of Services%2$s and %1$sPrivacy Policy%2$s. You may receive SMS/Email notifications from us and can opt out at any time.', 'core-functions' ), '<a class="" href="/privacy-policy/">', '</a>', '<a class="" href="/terms-of-services-therapist/">' ); ?>
 					</label>
