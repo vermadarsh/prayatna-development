1b771447cee6f486485fb4f646c1c4dff88ce779
Adarsh Verma <adarsh.srmcem@gmail.com>
2021-04-05 16:30:14 +0530

    added.

--- wp-content/plugins/core-functions/admin/templates/cpt-learning-lounge-log/new-student-log.php
+++ wp-content/plugins/core-functions/admin/templates/cpt-learning-lounge-log/new-student-log.php
@@ -11,5 +11,21 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly.
 ?>
 <div class="wrap">
 	<h1><?php esc_html_e( 'Register for Learning Lounge', 'core-functions' ); ?></h1>
-	
+	<form class="cf-new-learning-lounge-log-form" action="" method="POST">
+		<h4><?php esc_html_e( 'Fill in the details below to add a new log.', 'core-functions' ); ?></h4>
+		<table class="form-table">
+			<tbody>
+				<!-- FIELD: STUDENT NAME -->
+				<tr>
+					<th scope="row"><label for="student-name"><?php esc_html_e( 'Student Name', 'core-functions' ); ?></label></th>
+					<td>
+						<input type="text" name="student-name" id="student-name" class="regular-text" required />
+						<p class="cf-form-description-text"><?php esc_html_e( 'Your name goes here.', 'core-functions' ); ?></p>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		<?php wp_nonce_field( 'new-learning-lounge-log', 'new-learning-lounge-log-nonce' );
+		submit_button( __( 'Submit', 'core-functions' ), 'submit', 'add-new-learning-lounge-log' ); ?>
+	</form>
 </div>
