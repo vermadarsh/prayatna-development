c26c1817c3248b4c7ea8df19158138424c3a6fc2
Adarsh Verma <adarsh.srmcem@gmail.com>
2021-04-07 14:24:35 +0530

    added.

--- wp-content/plugins/core-functions/admin/templates/cpt-learning-lounge-log/payment-history.php
+++ wp-content/plugins/core-functions/admin/templates/cpt-learning-lounge-log/payment-history.php
@@ -98,9 +98,9 @@ class Cf_Payment_History_Table extends WP_List_Table {
 
 		$per_page     = $this->get_items_per_page( 'payment_history_data_per_page', 20 );
 		$current_page = $this->get_pagenum();
-		var_dump( get_current_user_id() ); die;
 		$payments_obj = cf_get_learning_lounge_logs( get_current_user_id() );
 		$payments     = $payments_obj->posts;
+		debug( $payments ); die;
 		$total_items  = count( $payments );
 
 		foreach ( $payments as $log_id ) {
