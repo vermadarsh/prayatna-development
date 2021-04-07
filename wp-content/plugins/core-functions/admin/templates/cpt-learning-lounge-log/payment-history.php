9c3424cfc4609c49c94ddbdca6ecdd6d5cea312d
Adarsh Verma <adarsh.srmcem@gmail.com>
2021-04-07 14:40:23 +0530

    added.

--- wp-content/plugins/core-functions/admin/templates/cpt-learning-lounge-log/payment-history.php
+++ wp-content/plugins/core-functions/admin/templates/cpt-learning-lounge-log/payment-history.php
@@ -113,6 +113,7 @@ class Cf_Payment_History_Table extends WP_List_Table {
 			$payment_date        = get_field( 'payment_date', $log_id );
 			$transaction_id      = get_field( 'transaction_id', $log_id );
 			$amount_paid_data    = sprintf( __( '%1$s (%3$s)%2$s', 'core-functions' ), $amount_paid, '<br />', $mode_of_payment, $bank_name, $payment_date, $transaction_id );
+			$record_added_date   = sprintf( __( 'Record added on %1$s at %2$s', 'core-functions' ), gmdate( 'F j, Y', strtotime( $log_post->post_date ) ), gmdate( 'H:i A', strtotime( $log_post->post_date ) ) );
 
 			$payment_history_table_data[] = array(
 				'author'              => $log_post->post_author,
