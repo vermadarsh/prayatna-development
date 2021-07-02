7b7e5b35bde2e5c9b0a18554fa8f041b7bfbd3c6
Adarsh Verma <nivsmehta4491@gmail.com>
2021-07-02 11:09:13 +0530

    add code

--- wp-content/plugins/core-functions/admin/class-core-functions-admin.php
+++ wp-content/plugins/core-functions/admin/class-core-functions-admin.php
@@ -252,7 +252,7 @@ class Core_Functions_Admin {
 							$unix2 = strtotime( $date1->format( 'Y-m-d' ) );
 							// debug($unix1);
 							// die;
-							$date2 = new DateTime( $leaveEndDate )
+							$date2 = new DateTime( $leaveEndDate );
 							$unix1 = strtotime( $date2->format( 'Y-m-d' ) );
 							$leaveReason      = get_field('reason_for_leave',$post_id);
 							if( 0 === ( $unix1 - $unix2 ) ) {
