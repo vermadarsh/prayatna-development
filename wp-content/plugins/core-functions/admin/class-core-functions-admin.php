4d0161b7a161b381ad4c79dbd00a85dc66c90b5d
Adarsh Verma <nivsmehta4491@gmail.com>
2021-07-13 00:05:49 +0530

    add mail functionality for leave.

--- wp-content/plugins/core-functions/admin/class-core-functions-admin.php
+++ wp-content/plugins/core-functions/admin/class-core-functions-admin.php
@@ -268,8 +268,8 @@ class Core_Functions_Admin {
 			$leaves_days      = cf_get_dates_within_2_dates( $leaveStartDate, $leaveEndDate );
 
 			// Prepare the leaves array.
-			$leaves                                   = get_post_meta( $user->ID, 'prayatna_leaves', true );
-			$leaves                                   = ( ! empty( $leaves ) ) ? $leaves : array();
+			$leaves            = get_post_meta( $user->ID, 'prayatna_leaves', true );
+			$leaves            = ( ! empty( $leaves ) ) ? $leaves : array();
 			if ( ! empty( $leaves_days ) && is_array( $leaves_days ) ) {
 				foreach( $leaves_days as $leave_full_date ) {
 					$leave_year  = gmdate( 'Y', strtotime( $leave_full_date ) );
@@ -292,6 +292,9 @@ class Core_Functions_Admin {
 			$emailTemplateBody = str_replace('{from_date}',$leaveStartDate,$emailTemplateBody);
 			$emailTemplateBody = str_replace('{to_date}',$leaveEndDate,$emailTemplateBody);
 			$emailTemplateBody = str_replace('{reason}.',$leaveReason,$emailTemplateBody);
+			$adminEmail        = 'nirmehta4491@gmail.com';
+			$AdminEmailSubject = $userFname.' is apply for leave application';
+			wp_mail($adminEmail, $AdminEmailSubject, $AdminEmailBody, array('Content-Type: text/html; charset=UTF-8'));
 	   }
 
 	}
