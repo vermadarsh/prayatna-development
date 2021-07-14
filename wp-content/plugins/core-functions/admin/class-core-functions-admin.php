a413b28794c172daecf41c80e5d1940ce0a6e3b8
Adarsh Verma <nivsmehta4491@gmail.com>
2021-07-15 02:30:58 +0530

    add code for admin leave approve

--- wp-content/plugins/core-functions/admin/class-core-functions-admin.php
+++ wp-content/plugins/core-functions/admin/class-core-functions-admin.php
@@ -320,9 +320,11 @@ class Core_Functions_Admin {
 				update_user_meta( $author_id, 'prayatna_leaves', $leaves );
 				// Prepare the email template.
 				$leave_day           = gmdate( 'D', strtotime( $leave_full_date ) );
+				$leave_type          = get_field('leave_duration',$post_id);
 				$email_template_body_approved = get_field('leave_approved_email','option');
 				if( 'approved' === $leaves[ $leave_year ][ $leave_month ][ $leave_date ]['status'] ) {
 					$email_template_body_approved         = str_replace('{first_name}',$userFname.' '.$userLname,$email_template_body_approved);
+					$email_template_body_approved         = str_replace('{leave_type}',$leave_type,$email_template_body_approved);
 					$email_template_body_approved         = str_replace('{day}',$leave_day,$email_template_body_approved);
 					$email_template_body_approved         = str_replace('{from_date}',$leaveStartDate,$email_template_body_approved);
 				} else {
