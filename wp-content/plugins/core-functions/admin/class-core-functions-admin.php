d4fe977aab0cd47a44aae47e2293e34fa2e4af48
Adarsh Verma <adarsh.srmcem@gmail.com>
2021-07-18 16:15:25 +0530

    added

--- wp-content/plugins/core-functions/admin/class-core-functions-admin.php
+++ wp-content/plugins/core-functions/admin/class-core-functions-admin.php
@@ -1439,8 +1439,10 @@ class Core_Functions_Admin {
 
 			// Convert the halfday leaves into fullday, if they are more than 1.
 			if ( 1 < $halfday_leaves ) {
-				var_dump( $halfday_leaves / 2 );
-				var_dump( $halfday_leaves % 2 );
+				$convertible_fullday_leaves = abs( $halfday_leaves / 2 );
+				$remaining_halfday_leaves   = $halfday_leaves % 2;
+				var_dump( $convertible_fullday_leaves );
+				var_dump( $remaining_halfday_leaves );
 				die;
 			}
 
