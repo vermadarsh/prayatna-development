47acbcc77d5b1da66bf2ac246db915afca295aa2
Adarsh Verma <nivsmehta4491@gmail.com>
2021-07-19 20:10:04 +0530

    add mail functionality for leave.

--- wp-content/plugins/core-functions/includes/class-salarypdf.php
+++ wp-content/plugins/core-functions/includes/class-salarypdf.php
@@ -6,16 +6,15 @@ class SALARYPDF extends TCPDF {
 //Page header
     public function Header() {
 	    $logo_image_id          = (int) get_field('site_logo','option');
-        debug($logo_image_id);
-        die;
-		$logo_image_url         = cf_get_image_url( $logo_image_id );
+        $logo_image_url         = cf_get_image_url( $logo_image_id );
 		$logo_file_info         = pathinfo( $logo_image_url );
 		$logo_file_extension    = ( ! empty( $logo_file_info['extension'] ) ) ? ucfirst( $logo_file_info['extension'] ) : '';
 		$this->SetFont( 'helvetica', 'M', 12 );
 		$this->SetXY( 140, 14 );
 		$this->writeHTMLCell( 0, 0, '', '', $html, 0, 1, 0, true, 'top', true );
 		$this->Image( $logo_image_url, 10, 5, 40, '', $logo_file_extension, '', 'T', false, 800, 'L', false, false, 0, false, false, false );
-
+        debug($this->Image);
+        die;
 		$html = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<table cellspacing="0" cellpadding="0" width="60%" border="0">
 					<tr width="100%">
 						<td style="line-height:14px;font-size:11px;">Salary Slip</td>
