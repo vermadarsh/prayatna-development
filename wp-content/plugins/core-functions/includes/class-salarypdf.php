96935a0099121580b89cc04aed484e1c1d185955
Adarsh Verma <nivsmehta4491@gmail.com>
2021-07-19 19:53:34 +0530

    add mail functionality for leave.

--- wp-content/plugins/core-functions/includes/class-salarypdf.php
+++ wp-content/plugins/core-functions/includes/class-salarypdf.php
@@ -5,36 +5,13 @@ class SALARYPDF extends TCPDF {
 
 //Page header
     public function Header() {
-	    // $logo_image_id          = (int) wpir_get_plugin_setting( 'store-logo-id' );
-		$logo_image_url         = get_field('site_logo','option');
-		$logo_file_info         = pathinfo( $logo_image_url );
-        // debug($logo_file_info);
-        // die;
-		$logo_file_extension    = ( ! empty( $logo_file_info['extension'] ) ) ? ucfirst( $logo_file_info['extension'] ) : '';
-		// $store_address          = wpir_get_store_formatted_address();
-
-		// Store Information.
-		// $store_contact = wpir_get_plugin_setting( 'store-contact-number' );
-		$store_website = site_url();
-		$store_email   = get_option( 'admin_email' );
-
-		$this->SetFont( 'helvetica', 'B', 12 );
-		$this->SetXY( 140, 14 );
-		$this->writeHTMLCell( 0, 0, '', '', $html, 0, 1, 0, true, 'top', true );
-		$this->Image( $logo_image_url, 10, 5, 40, '', $logo_file_extension, '', 'T', false, 800, 'L', false, false, 0, false, false, false );
-
-		$html = '';
-
-		$this->SetXY( 50, 5 );
-		$this->writeHTMLCell( 0, 0, '', '', $html, 0, 1, 0, true, 'top', true );
-		$style = array(
-			'width' => 0.25,
-			'cap'   => 'butt',
-			'join'  => 'miter',
-			'dash'  => 0,
-			'color' => array( 222, 219, 219 ),
-		);
-		$this->Line( 0, 30, 220, 30, $style );
+	    // Logo
+		$image_file = get_field('site_logo','option');
+		$this->Image($image_file, 10, 10, 15, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
+		// Set font
+		$this->SetFont('helvetica', 'B', 20);
+		// Title
+		$this->Cell(0, 15, '<< TCPDF Example 003 >>', 0, false, 'C', 0, '', 0, false, 'M', 'M');
 	}
 	public function Footer() {
 	    // Position at 15 mm from bottom
