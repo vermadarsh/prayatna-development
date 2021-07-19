af8684ec93c86ae077616f244ed73747d09bf292
Adarsh Verma <nivsmehta4491@gmail.com>
2021-07-19 20:06:55 +0530

    add mail functionality for leave.

--- wp-content/plugins/core-functions/includes/class-salarypdf.php
+++ wp-content/plugins/core-functions/includes/class-salarypdf.php
@@ -5,20 +5,31 @@ class SALARYPDF extends TCPDF {
 
 //Page header
     public function Header() {
-	    // Logo
-		$image_id            = get_field('site_logo','option');
-        $image_url           = cf_get_image_url( $image_id );
-        $logo_file_info      = pathinfo( $image_file );
-        // debug($logo_file_info);
-        // die;
-		$logo_file_extension = ( ! empty( $logo_file_info['extension'] ) ) ? ucfirst( $logo_file_info['extension'] ) : '';
-        $this->Image( $image_url, 10, 5, 40, '', $logo_file_extension, '', 'T', false, 800, 'L', false, false, 0, false, false, false );
-		// Set font
-		$this->SetFont('helvetica', 'B', 20);
-		// Title
-		$this->Cell(0, 15, 'Salary Slip', 0, false, 'C', 0, '', 0, false, 'M', 'M');
-        // debug($this);
-        // die;
+	    $logo_image_id          = (int) get_field('site_logo','option');
+		$logo_image_url         = cf_get_image_url( $logo_image_id );
+		$logo_file_info         = pathinfo( $logo_image_url );
+		$logo_file_extension    = ( ! empty( $logo_file_info['extension'] ) ) ? ucfirst( $logo_file_info['extension'] ) : '';
+		$this->SetFont( 'helvetica', 'M', 12 );
+		$this->SetXY( 140, 14 );
+		$this->writeHTMLCell( 0, 0, '', '', $html, 0, 1, 0, true, 'top', true );
+		$this->Image( $logo_image_url, 10, 5, 40, '', $logo_file_extension, '', 'T', false, 800, 'L', false, false, 0, false, false, false );
+
+		$html = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<table cellspacing="0" cellpadding="0" width="60%" border="0">
+					<tr width="100%">
+						<td style="line-height:14px;font-size:11px;">Salary Slip</td>
+					</tr>
+				</table>';
+
+		$this->SetXY( 50, 5 );
+		$this->writeHTMLCell( 0, 0, '', '', $html, 0, 1, 0, true, 'top', true );
+		$style = array(
+			'width' => 0.25,
+			'cap'   => 'butt',
+			'join'  => 'miter',
+			'dash'  => 0,
+			'color' => array( 222, 219, 219 ),
+		);
+		$this->Line( 0, 30, 220, 30, $style );
 	}
 	public function Footer() {
 	    // Position at 15 mm from bottom
