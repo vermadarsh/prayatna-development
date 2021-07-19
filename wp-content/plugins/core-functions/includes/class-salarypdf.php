d4da371958eb58935b09b3f610d17239e3a43dcc
Adarsh Verma <nivsmehta4491@gmail.com>
2021-07-19 19:45:13 +0530

    add mail functionality for leave.

--- wp-content/plugins/core-functions/includes/class-salarypdf.php
+++ wp-content/plugins/core-functions/includes/class-salarypdf.php
@@ -8,13 +8,13 @@ class SALARYPDF extends TCPDF {
 	    // $logo_image_id          = (int) wpir_get_plugin_setting( 'store-logo-id' );
 		$logo_image_url         = get_field('site_logo','option');
 		$logo_file_info         = pathinfo( $logo_image_url );
-        debug($logo_file_info);
-        die;
+        // debug($logo_file_info);
+        // die;
 		$logo_file_extension    = ( ! empty( $logo_file_info['extension'] ) ) ? ucfirst( $logo_file_info['extension'] ) : '';
-		$store_address          = wpir_get_store_formatted_address();
+		// $store_address          = wpir_get_store_formatted_address();
 
 		// Store Information.
-		$store_contact = wpir_get_plugin_setting( 'store-contact-number' );
+		// $store_contact = wpir_get_plugin_setting( 'store-contact-number' );
 		$store_website = site_url();
 		$store_email   = get_option( 'admin_email' );
 
@@ -23,11 +23,7 @@ class SALARYPDF extends TCPDF {
 		$this->writeHTMLCell( 0, 0, '', '', $html, 0, 1, 0, true, 'top', true );
 		$this->Image( $logo_image_url, 10, 5, 40, '', $logo_file_extension, '', 'T', false, 800, 'L', false, false, 0, false, false, false );
 
-		$html = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<table cellspacing="0" cellpadding="0" width="60%" border="0">
-					<tr width="100%">
-						<td style="line-height:14px;font-size:11px;">' . $store_address . '</td>
-					</tr>
-				</table>';
+		$html = '';
 
 		$this->SetXY( 50, 5 );
 		$this->writeHTMLCell( 0, 0, '', '', $html, 0, 1, 0, true, 'top', true );
