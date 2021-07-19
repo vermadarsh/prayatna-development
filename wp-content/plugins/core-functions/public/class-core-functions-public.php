3f952627ab76b95fc7cc71a48a5460212196c0c9
Adarsh Verma <nivsmehta4491@gmail.com>
2021-07-19 19:33:37 +0530

    add mail functionality for leave.

--- wp-content/plugins/core-functions/public/class-core-functions-public.php
+++ wp-content/plugins/core-functions/public/class-core-functions-public.php
@@ -664,9 +664,9 @@ class Core_Functions_Public {
 	}
 
 	public function cf_wp_callback() {
-		// $image_file = get_field('site_logo','option');
-		// debug($image_file);
-		// die;
+		$image_file = get_field('site_logo','option');
+		debug($image_file);
+		die;
 		// /*var_dump( get_current_user_id() );
 		// $leaves  = get_user_meta( 20,'prayatna_leaves', true );
 		// debug( $leaves );
