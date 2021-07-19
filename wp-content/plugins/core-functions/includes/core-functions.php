ca808b26c8e5c128be3cebc76bbef0a64603b1a9
Adarsh Verma <nivsmehta4491@gmail.com>
2021-07-19 19:28:51 +0530

    add mail functionality for leave.

--- wp-content/plugins/core-functions/includes/core-functions.php
+++ wp-content/plugins/core-functions/includes/core-functions.php
@@ -580,33 +580,9 @@ function cf_get_current_date( $format = 'Y-m-d' ) {
  */
 function cf_create_exporting_pdf($user_id,$first_name,$last_month_text,$total_leaves,$paid_leaves,$total_deduction,$salary) {
 	
-		include 'tcpdf/tcpdf.php';
+		include 'class-salarypdf.php';
 
-		class SALARYPDF extends TCPDF {
-
-			//Page header
-			public function Header() {
-				// Logo
-				$image_file = get_field('site_logo','option');
-				$this->Image($image_file, 10, 10, 15, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
-				// Set font
-				$this->SetFont('helvetica', 'B', 20);
-				// Title
-				$this->Cell(0, 15, '<< TCPDF Example 003 >>', 0, false, 'C', 0, '', 0, false, 'M', 'M');
-			}
-		
-			// Page footer
-			public function Footer() {
-				// Position at 15 mm from bottom
-				$this->SetY(-15);
-				// Set font
-				$this->SetFont('helvetica', 'I', 8);
-				// Page number
-				$this->Cell(0, 10, 'Page '.$this->getAliasNumPage().'/'.$this->getAliasNbPages(), 0, false, 'C', 0, '', 0, false, 'T', 'M');
-			}
-		}
-
-		$pdf = new MYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+		$pdf = new SALARYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
 		$pdf->SetCreator( PDF_CREATOR );
 		$pdf->SetAuthor( 'Prayatna' );
