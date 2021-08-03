31bd281ce1b92a8000802d78c1165c9587d93e5e
Adarsh Verma <nivsmehta4491@gmail.com>
2021-08-04 03:56:07 +0530

    add mail functionality for leave.

--- wp-content/plugins/core-functions/public/templates/registration/therapist.php
+++ wp-content/plugins/core-functions/public/templates/registration/therapist.php
@@ -103,7 +103,7 @@ $heading = ( ! empty( $args['heading'] ) ) ? $args['heading'] : '';
 				<div class="custom-control custom-checkbox therapist-registration-acceptance">
 					<input type="checkbox" class="custom-control-input" id="therapist-registration-terms-n-conditions-acceptance" />
 					<label class="custom-control-label" for="therapist-registration-terms-n-conditions-acceptance">
-						<?php echo sprintf( __( 'By clicking Sign Up, you agree to our %3$sTerms of Services%2$s and %1$sPrivacy Policy%2$s. You may receive SMS/Email notifications from us and can opt out at any time.', 'core-functions' ), '<a class="" href="/privacy-policy/">', '</a>', '<a class="" href="/terms-of-services-therapist/">' ); ?>
+						<?php echo sprintf( __( 'By clicking Register as Therapist, you agree to our %3$sTerms of Services%2$s and %1$sPrivacy Policy%2$s. You may receive SMS/Email notifications from us and can opt out at any time.', 'core-functions' ), '<a class="" href="/privacy-policy/">', '</a>', '<a class="" href="/terms-of-services-therapist/">' ); ?>
 					</label>
 				</div>
 			</div>
